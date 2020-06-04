5fef32d77f01ca5fafcc3dbca89e151a2537806c
Jeremiah Black <jeremiahblackol@gmail.com>
2020-06-03 21:03:31 -0600

    added trips test data set

---
+++ test-data/trips-test-data.js
@@ -0,0 +1,331 @@
+const trips = [{
+  "id": 1,
+  "userID": 1,
+  "destinationID": 49,
+  "travelers": 1,
+  "date": "2019/09/16",
+  "duration": 8,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 2,
+  "userID": 13,
+  "destinationID": 25,
+  "travelers": 5,
+  "date": "2020/10/04",
+  "duration": 18,
+  "status": "pending",
+  "suggestedActivities": []
+},
+{
+  "id": 3,
+  "userID": 12,
+  "destinationID": 22,
+  "travelers": 4,
+  "date": "2020/05/22",
+  "duration": 17,
+  "status": "pending",
+  "suggestedActivities": []
+},
+{
+  "id": 4,
+  "userID": 3,
+  "destinationID": 14,
+  "travelers": 2,
+  "date": "2020/02/25",
+  "duration": 10,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 5,
+  "userID": 4,
+  "destinationID": 29,
+  "travelers": 3,
+  "date": "2020/04/30",
+  "duration": 18,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 6,
+  "userID": 9,
+  "destinationID": 35,
+  "travelers": 3,
+  "date": "2020/06/29",
+  "duration": 9,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 7,
+  "userID": 7,
+  "destinationID": 17,
+  "travelers": 5,
+  "date": "2020/5/28",
+  "duration": 20,
+  "status": "approved",
+  "suggestedActivities": []
+},{
+  "id": 8,
+  "userID": 16,
+  "destinationID": 39,
+  "travelers": 6,
+  "date": "2021/02/07",
+  "duration": 4,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 9,
+  "userID": 14,
+  "destinationID": 19,
+  "travelers": 5,
+  "date": "2019/12/19",
+  "duration": 19,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 10,
+  "userID": 19,
+  "destinationID": 50,
+  "travelers": 6,
+  "date": "2020/07/23",
+  "duration": 17,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 11,
+  "userID": 5,
+  "destinationID": 5,
+  "travelers": 4,
+  "date": "2020/10/14",
+  "duration": 4,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 12,
+  "userID": 3,
+  "destinationID": 33,
+  "travelers": 6,
+  "date": "2020/10/17",
+  "duration": 6,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 13,
+  "userID": 14,
+  "destinationID": 12,
+  "travelers": 1,
+  "date": "2021/02/12",
+  "duration": 11,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 14,
+  "userID": 19,
+  "destinationID": 35,
+  "travelers": 1,
+  "date": "2020/09/24",
+  "duration": 10,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 15,
+  "userID": 5,
+  "destinationID": 13,
+  "travelers": 3,
+  "date": "2020/07/04",
+  "duration": 6,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 16,
+  "userID": 19,
+  "destinationID": 27,
+  "travelers": 1,
+  "date": "2019/11/20",
+  "duration": 9,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 17,
+  "userID": 8,
+  "destinationID": 31,
+  "travelers": 1,
+  "date": "2019/10/30",
+  "duration": 20,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 18,
+  "userID": 11,
+  "destinationID": 2,
+  "travelers": 2,
+  "date": "2020/09/25",
+  "duration": 17,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 19,
+  "userID": 7,
+  "destinationID": 47,
+  "travelers": 4,
+  "date": "2019/07/21",
+  "duration": 5,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 20,
+  "userID": 10,
+  "destinationID": 19,
+  "travelers": 4,
+  "date": "2020/10/05",
+  "duration": 6,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 21,
+  "userID": 1,
+  "destinationID": 10,
+  "travelers": 1,
+  "date": "2020/01/28",
+  "duration": 18,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 22,
+  "userID": 12,
+  "destinationID": 9,
+  "travelers": 4,
+  "date": "2020/05/01",
+  "duration": 19,
+  "status": "pending",
+  "suggestedActivities": []
+},
+{
+  "id": 23,
+  "userID": 15,
+  "destinationID": 24,
+  "travelers": 6,
+  "date": "2020/01/02",
+  "duration": 18,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 24,
+  "userID": 4,
+  "destinationID": 26,
+  "travelers": 5,
+  "date": "2019/11/15",
+  "duration": 7,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 25,
+  "userID": 8,
+  "destinationID": 12,
+  "travelers": 6,
+  "date": "2019/10/26",
+  "duration": 9,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 26,
+  "userID": 5,
+  "destinationID": 15,
+  "travelers": 3,
+  "date": "2019/12/10",
+  "duration": 9,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 27,
+  "userID": 3,
+  "destinationID": 7,
+  "travelers": 6,
+  "date": "2019/07/16",
+  "duration": 5,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 28,
+  "userID": 8,
+  "destinationID": 31,
+  "travelers": 3,
+  "date": "2020/02/03",
+  "duration": 14,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 29,
+  "userID": 17,
+  "destinationID": 25,
+  "travelers": 3,
+  "date": "2019/08/30",
+  "duration": 4,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 30,
+  "userID": 13,
+  "destinationID": 29,
+  "travelers": 1,
+  "date": "2020/07/17",
+  "duration": 5,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 31,
+  "userID": 11,
+  "destinationID": 33,
+  "travelers": 3,
+  "date": "2020/12/19",
+  "duration": 15,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 32,
+  "userID": 4,
+  "destinationID": 36,
+  "travelers": 5,
+  "date": "2020/05/06",
+  "duration": 6,
+  "status": "approved",
+  "suggestedActivities": []
+},
+{
+  "id": 33,
+  "userID": 6,
+  "destinationID": 36,
+  "travelers": 5,
+  "date": "2020/03/26",
+  "duration": 19,
+  "status": "approved",
+  "suggestedActivities": []
+}]
+
+export default trips;
